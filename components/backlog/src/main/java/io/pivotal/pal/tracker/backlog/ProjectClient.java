9b0e5869d2a40ce964c28d19d49fc69b775b2b83
milpo <aneeshkumar10@gmail.com>
2020-03-21 15:01:25 -0400

    cicuit breakers

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,6 +1,8 @@
 package io.pivotal.pal.tracker.backlog;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
 
 import java.util.Map;
